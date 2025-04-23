485a50ea9239e614c4f068acc17fcc6007f332d5
Nagarjun5005 <nagarjun20n@gmail.com>
2025-04-23 21:41:31 +0530

    initial commit

---
+++ 01-starting-project/src/app/user-input/user-input.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+    enteredInitialInvestment="0";
+    enteredAnnualInvestment="0";
+    enteredDuration="10";
+    enteredExpectedReturn="5";
+
+  onSubmit(){
+    console.log("submitted");
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredDuration);
+    console.log(this.enteredExpectedReturn);
+  }
+}
